d42077674275b8a7e63fa1fd973eecf52a89a2fc
Joneel <joneel@nyftylabs.com>
2024-08-02 16:08:23 +0800

    -Custom Post Pagination

--- hello-theme-child/widgets/custom-post-widget.php
+++ hello-theme-child/widgets/custom-post-widget.php
@@ -2,6 +2,8 @@
 use Elementor\Controls_Manager;
 use Elementor\Group_Control_Image_Size;
 use Elementor\Icons_Manager;
+use Elementor\Group_Control_Typography;
+use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
 use Elementor\Utils;
 use Elementor\Widget_Base;
 use ElementorPro\Modules\QueryControl\Controls\Group_Control_Related;
@@ -117,6 +119,8 @@ class Elementor_Custom_Post_Widget extends Widget_Base {
         Post_Style::register_design_image_controls($this);
 
         Post_Style::register_design_content_controls($this);
+
+        $this->register_pagination_section_controls();
     }
 
     protected function register_query_section_controls() {
@@ -165,6 +169,7 @@ class Elementor_Custom_Post_Widget extends Widget_Base {
 
         $query_args = [
             'posts_per_page' => $this->get_settings( 'posts_per_page' ),
+            'paged' => $this->get_current_page(),
         ];
 
         /** @var Module_Query $elementor_query */
@@ -351,6 +356,57 @@ class Elementor_Custom_Post_Widget extends Widget_Base {
         ?>
         </div>
         <?php
+        if ( '' === $this->get_settings_for_display('pagination_type') ) {
+            return;
+        }
+
+        $page_limit = $this->get_query()->max_num_pages;
+
+        //var_dump($page_limit);
+        $current_page = $this->get_current_page();
+
+        $settings = $this->get_settings_for_display();
+
+        $this->add_render_attribute( 'pagination', 'class', 'elementor-pagination' );
+        $has_numbers = in_array( $settings['pagination_type'], [ 'numbers', 'numbers_and_prev_next' ] );
+        $has_prev_next = in_array( $settings['pagination_type'], [ 'prev_next', 'numbers_and_prev_next' ] );
+
+        $links = [];
+        if ( $has_numbers ) {
+            $paginate_args = [
+                'type' => 'array',
+                'current' => $current_page,
+                'total' => $page_limit,
+                'prev_next' => false,
+                'show_all' => 'yes' !== $settings['pagination_numbers_shorten'],
+                'before_page_number' => '<span class="elementor-screen-only">' . esc_html__( 'Page', 'elementor-pro' ) . '</span>',
+            ];
+
+            if ( is_singular() && ! is_front_page() ) {
+                global $wp_rewrite;
+                if ( $wp_rewrite->using_permalinks() ) {
+                    $paginate_args['base'] = trailingslashit( get_permalink() ) . '%_%';
+                    $paginate_args['format'] = user_trailingslashit( '%#%', 'single_paged' );
+                } else {
+                    $paginate_args['format'] = '?page=%#%';
+                }
+            }
+
+            $links = paginate_links( $paginate_args );
+        }
+
+        if ( $has_prev_next ) {
+            $prev_next = $this->get_posts_nav_link($page_limit);
+            array_unshift( $links, $prev_next['prev'] );
+            $links[] = $prev_next['next'];
+        }
+
+        // PHPCS - Seems that `$links` is safe.
+        ?>
+        <nav class="elementor-pagination" aria-label="<?php esc_attr_e( 'Pagination', 'elementor-pro' ); ?>">
+            <?php echo implode( PHP_EOL, $links ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+        </nav>
+        <?php
     }
 
     public function render() {
@@ -371,9 +427,373 @@ class Elementor_Custom_Post_Widget extends Widget_Base {
             $this->render_post();
         }
 
-        $this->render_loop_footer();
-
         wp_reset_postdata();
+
+        $this->render_loop_footer();
+    }
+
+    public function register_pagination_section_controls() {
+        $this->start_controls_section(
+            'section_pagination',
+            [
+                'label' => esc_html__( 'Pagination', 'elementor-pro' ),
+            ]
+        );
+
+        $this->add_control(
+            'pagination_type',
+            [
+                'label' => esc_html__( 'Pagination', 'elementor-pro' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => '',
+                'options' => $this->get_pagination_type_options(),
+                'frontend_available' => true,
+            ]
+        );
+
+        $this->add_control(
+            'pagination_page_limit',
+            [
+                'label' => esc_html__( 'Page Limit', 'elementor-pro' ),
+                'default' => '5',
+                'condition' => [
+                    'pagination_type!' => [
+                        'load_more_on_click',
+                        'load_more_infinite_scroll',
+                        '',
+                    ],
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'pagination_numbers_shorten',
+            [
+                'label' => esc_html__( 'Shorten', 'elementor-pro' ),
+                'type' => Controls_Manager::SWITCHER,
+                'default' => '',
+                'condition' => [
+                    'pagination_type' => [
+                        'numbers',
+                        'numbers_and_prev_next',
+                    ],
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'pagination_prev_label',
+            [
+                'label' => esc_html__( 'Previous Label', 'elementor-pro' ),
+                'dynamic' => [
+                    'active' => true,
+                ],
+                'default' => esc_html__( '&laquo; Previous', 'elementor-pro' ),
+                'condition' => [
+                    'pagination_type' => [
+                        'prev_next',
+                        'numbers_and_prev_next',
+                    ],
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'pagination_next_label',
+            [
+                'label' => esc_html__( 'Next Label', 'elementor-pro' ),
+                'default' => esc_html__( 'Next &raquo;', 'elementor-pro' ),
+                'condition' => [
+                    'pagination_type' => [
+                        'prev_next',
+                        'numbers_and_prev_next',
+                    ],
+                ],
+                'dynamic' => [
+                    'active' => true,
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'pagination_align',
+            [
+                'label' => esc_html__( 'Alignment', 'elementor-pro' ),
+                'type' => Controls_Manager::CHOOSE,
+                'options' => [
+                    'left' => [
+                        'title' => esc_html__( 'Left', 'elementor-pro' ),
+                        'icon' => 'eicon-text-align-left',
+                    ],
+                    'center' => [
+                        'title' => esc_html__( 'Center', 'elementor-pro' ),
+                        'icon' => 'eicon-text-align-center',
+                    ],
+                    'right' => [
+                        'title' => esc_html__( 'Right', 'elementor-pro' ),
+                        'icon' => 'eicon-text-align-right',
+                    ],
+                ],
+                'default' => 'center',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-pagination' => 'text-align: {{VALUE}};',
+                ],
+                'condition' => [
+                    'pagination_type!' => [
+                        'load_more_on_click',
+                        'load_more_infinite_scroll',
+                        '',
+                    ],
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        // Pagination style controls for prev/next and numbers pagination.
+        $this->start_controls_section(
+            'section_pagination_style',
+            [
+                'label' => esc_html__( 'Pagination', 'elementor-pro' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+                'condition' => [
+                    'pagination_type!' => [
+                        'load_more_on_click',
+                        'load_more_infinite_scroll',
+                        '',
+                    ],
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            Group_Control_Typography::get_type(),
+            [
+                'name' => 'pagination_typography',
+                'selector' => '{{WRAPPER}} .elementor-pagination',
+                'global' => [
+                    'default' => Global_Typography::TYPOGRAPHY_SECONDARY,
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'pagination_color_heading',
+            [
+                'label' => esc_html__( 'Colors', 'elementor-pro' ),
+                'type' => Controls_Manager::HEADING,
+                'separator' => 'before',
+            ]
+        );
+
+        $this->start_controls_tabs( 'pagination_colors' );
+
+        $this->start_controls_tab(
+            'pagination_color_normal',
+            [
+                'label' => esc_html__( 'Normal', 'elementor-pro' ),
+            ]
+        );
+
+        $this->add_control(
+            'pagination_color',
+            [
+                'label' => esc_html__( 'Color', 'elementor-pro' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-pagination .page-numbers:not(.dots)' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_tab();
+
+        $this->start_controls_tab(
+            'pagination_color_hover',
+            [
+                'label' => esc_html__( 'Hover', 'elementor-pro' ),
+            ]
+        );
+
+        $this->add_control(
+            'pagination_hover_color',
+            [
+                'label' => esc_html__( 'Color', 'elementor-pro' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-pagination a.page-numbers:hover' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_tab();
+
+        $this->start_controls_tab(
+            'pagination_color_active',
+            [
+                'label' => esc_html__( 'Active', 'elementor-pro' ),
+            ]
+        );
+
+        $this->add_control(
+            'pagination_active_color',
+            [
+                'label' => esc_html__( 'Color', 'elementor-pro' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-pagination .page-numbers.current' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_tab();
+
+        $this->end_controls_tabs();
+
+        $this->add_responsive_control(
+            'pagination_spacing',
+            [
+                'label' => esc_html__( 'Space Between', 'elementor-pro' ),
+                'type' => Controls_Manager::SLIDER,
+                'separator' => 'before',
+                'default' => [
+                    'size' => 10,
+                ],
+                'range' => [
+                    'px' => [
+                        'min' => 0,
+                        'max' => 100,
+                    ],
+                ],
+                'selectors' => [
+                    'body:not(.rtl) {{WRAPPER}} .elementor-pagination .page-numbers:not(:first-child)' => 'margin-left: calc( {{SIZE}}{{UNIT}}/2 );',
+                    'body:not(.rtl) {{WRAPPER}} .elementor-pagination .page-numbers:not(:last-child)' => 'margin-right: calc( {{SIZE}}{{UNIT}}/2 );',
+                    'body.rtl {{WRAPPER}} .elementor-pagination .page-numbers:not(:first-child)' => 'margin-right: calc( {{SIZE}}{{UNIT}}/2 );',
+                    'body.rtl {{WRAPPER}} .elementor-pagination .page-numbers:not(:last-child)' => 'margin-left: calc( {{SIZE}}{{UNIT}}/2 );',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'pagination_spacing_top',
+            [
+                'label' => esc_html__( 'Spacing', 'elementor-pro' ),
+                'type' => Controls_Manager::SLIDER,
+                'range' => [
+                    'px' => [
+                        'min' => 0,
+                        'max' => 100,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-pagination' => 'margin-top: {{SIZE}}{{UNIT}}',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        // Pagination style controls for on-load pagination with type on-click/infinity-scroll.
+        $this->start_controls_section(
+            'section_style',
+            [
+                'label' => esc_html__( 'Pagination', 'elementor-pro' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+                'condition' => [
+                    'pagination_type' => [
+                        'load_more_on_click',
+                        'load_more_infinite_scroll',
+                    ],
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    public function get_current_page() {
+        if ( '' === $this->get_settings_for_display( 'pagination_type' ) ) {
+            return 1;
+        }
+
+        return max( 1, get_query_var( 'paged' ), get_query_var( 'page' ) );
+    }
+
+    public function get_wp_link_page( $i ) {
+        if ( ! is_singular() || is_front_page() ) {
+            return get_pagenum_link( $i );
+        }
+
+        // Based on wp-includes/post-template.php:957 `_wp_link_page`.
+        global $wp_rewrite;
+        $post = get_post();
+        $query_args = [];
+        $url = get_permalink();
+
+        if ( $i > 1 ) {
+            if ( '' === get_option( 'permalink_structure' ) || in_array( $post->post_status, [ 'draft', 'pending' ] ) ) {
+                $url = add_query_arg( 'page', $i, $url );
+            } elseif ( get_option( 'show_on_front' ) === 'page' && (int) get_option( 'page_on_front' ) === $post->ID ) {
+                $url = trailingslashit( $url ) . user_trailingslashit( "$wp_rewrite->pagination_base/" . $i, 'single_paged' );
+            } else {
+                $url = trailingslashit( $url ) . user_trailingslashit( $i, 'single_paged' );
+            }
+        }
+
+        if ( is_preview() ) {
+            if ( ( 'draft' !== $post->post_status ) && isset( $_GET['preview_id'], $_GET['preview_nonce'] ) ) {
+                $query_args['preview_id'] = wp_unslash( $_GET['preview_id'] );
+                $query_args['preview_nonce'] = wp_unslash( $_GET['preview_nonce'] );
+            }
+
+            $url = get_preview_post_link( $post, $query_args, $url );
+        }
+
+        return $url;
+    }
+
+    public function get_posts_nav_link( $page_limit = null ) {
+        if ( ! $page_limit ) {
+            $page_limit = $this->get_query()->max_num_pages;
+
+        }
+
+        $return = [];
+
+        $paged = $this->get_current_page();
+
+        $link_template = '<a class="page-numbers %s" href="%s">%s</a>';
+        $disabled_template = '<span class="page-numbers %s">%s</span>';
+
+        if ( $paged > 1 ) {
+            $next_page = intval( $paged ) - 1;
+            if ( $next_page < 1 ) {
+                $next_page = 1;
+            }
+
+            $return['prev'] = sprintf( $link_template, 'prev', $this->get_wp_link_page( $next_page ), $this->get_settings_for_display( 'pagination_prev_label' ) );
+        } else {
+            $return['prev'] = sprintf( $disabled_template, 'prev', $this->get_settings_for_display( 'pagination_prev_label' ) );
+        }
+
+        $next_page = intval( $paged ) + 1;
+
+        if ( $next_page <= $page_limit ) {
+            $return['next'] = sprintf( $link_template, 'next', $this->get_wp_link_page( $next_page ), $this->get_settings_for_display( 'pagination_next_label' ) );
+        } else {
+            $return['next'] = sprintf( $disabled_template, 'next', $this->get_settings_for_display( 'pagination_next_label' ) );
+        }
+
+        return $return;
+    }
+
+    protected function get_pagination_type_options() {
+        return [
+            '' => esc_html__( 'None', 'elementor-pro' ),
+            'numbers' => esc_html__( 'Numbers', 'elementor-pro' ),
+            'prev_next' => esc_html__( 'Previous/Next', 'elementor-pro' ),
+            'numbers_and_prev_next' => esc_html__( 'Numbers', 'elementor-pro' ) . ' + ' . esc_html__( 'Previous/Next', 'elementor-pro' ),
+        ];
     }
 
 }
